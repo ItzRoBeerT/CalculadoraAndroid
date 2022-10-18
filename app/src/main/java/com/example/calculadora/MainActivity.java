c7f3162313a1d160df4e4d2b6d7717fb8aef9214
Roberto <105317145+ItzRoBeerT@users.noreply.github.com>
2022-10-18 19:31:12 +0200

    calculadora acabada

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     public String sumatorioCadena="";
+    Double resultado=0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +28,39 @@ public class MainActivity extends AppCompatActivity {
         TextView txtV = findViewById(R.id.txtNumeros);
         Button b = (Button) v;
 
-        if(b.getText().equals("=")){
-            Double resultado = eval(sumatorioCadena);
+
+        if(b.getText().equals("MC")){
+            resultado=0.0;
+            sumatorioCadena=resultado+"";
+            txtV.setText(resultado+"");
+        }
+        else if(b.getText().equals("MR")){
+            sumatorioCadena=resultado+"";
+            txtV.setText(resultado+"");
+
+        }
+        else if(b.getText().equals("M+")){
+            resultado+= Double.parseDouble((String)txtV.getText());
+            sumatorioCadena="";
+            txtV.setText("");
+        }
+        else if(b.getText().equals("M-")){
+            resultado-= Double.parseDouble((String)txtV.getText());
+            sumatorioCadena="";
+            txtV.setText("");
+        }
+        else if(b.getText().equals("+/-")){
+            resultado= -resultado;
+            sumatorioCadena="";
+            txtV.setText(resultado+"");
+        }
+        else if(b.getText().equals("C")){
+            resultado=0.0;
+            sumatorioCadena="";
+            txtV.setText("");
+        }
+        else if(b.getText().equals("=")){
+             resultado = eval(sumatorioCadena);
             txtV.setText(resultado+"");
             sumatorioCadena="";
         }else{
@@ -36,145 +68,6 @@ public class MainActivity extends AppCompatActivity {
             txtV.setText(sumatorioCadena);
         }
     }
-
-    public int calcular(String operacion){
-
-        ArrayList<Character> listaNumeros = new ArrayList<Character>();
-        ArrayList<Character> listaOperadores = new ArrayList<>();
-        ArrayList<Integer> indicesOperaciones= new ArrayList<>();
-        int resultado=0, index=0, indexOp=0;
-
-
-        for (Character caracter : operacion.toCharArray()) {
-
-            if(caracter >= 48 && caracter <=57){
-                index++;
-                listaNumeros.add(caracter);
-            }else{
-                indexOp=index;
-                indicesOperaciones.add(index);
-                listaOperadores.add(caracter);
-            }
-
-        }
-
-        int indexO=0;
-        for(Character op: listaOperadores){
-
-            if(op.equals('+')){
-              resultado= sumar(listaNumeros, indicesOperaciones.get(indexO));
-              indexO++;
-            }
-            else if(op.equals('-')){
-                if(resultado==0) resultado= restar(listaNumeros, indicesOperaciones.get(indexO));
-                else{
-                   resultado= restar(resultado, listaNumeros,5);
-                }
-            }
-            else if(op.equals('X')){
-                if(resultado==0) resultado= mult(listaNumeros, indexOp);
-
-            }
-            else if(op.equals('/')){
-                if(resultado==0) resultado= div(listaNumeros, indexOp);
-            }
-            indexO++;
-
-        }
-        return resultado;
-    }
-
-    public int sumar(ArrayList<Character> numeros, int indOp){
-
-        String numero1="";
-        String numero2="";
-        int resultado = 0;
-
-        for(int i = 0; i<indOp; i++){
-            numero1+=numeros.get(i);
-        }
-        for(int i = indOp; i<numeros.size(); i++){
-            numero2+=numeros.get(i);
-        }
-        Integer n1= Integer.parseInt(numero1);
-        Integer n2 = Integer.parseInt(numero2);
-        resultado = n1+n2;
-
-        Character a = (char) resultado;
-        return resultado;
-    }
-
-    public int restar(ArrayList<Character> numeros, int indOp){
-
-        String numero1="";
-        String numero2="";
-        int resultado = 0;
-
-        for(int i = 0; i<indOp; i++){
-            numero1+=numeros.get(i);
-        }
-        for(int i = indOp; i<numeros.size(); i++){
-            numero2+=numeros.get(i);
-        }
-        Integer n1= Integer.parseInt(numero1);
-        Integer n2 = Integer.parseInt(numero2);
-        resultado = n1-n2;
-
-        Character a = (char) resultado;
-        return resultado;
-    }
-    public int restar(int resul,ArrayList<Character> numeros, int indOp ){
-
-        int resultado=0;
-
-        String numero2="";
-        for(int i = indOp; i<numeros.size(); i++){
-            numero2+=numeros.get(i);
-        }
-        Integer numeroRestar= Integer.parseInt(numero2);
-        resultado= resul- numeroRestar;
-        return resultado;
-    }
-    public int mult(ArrayList<Character> numeros, int indOp){
-
-        String numero1="";
-        String numero2="";
-        int resultado = 0;
-
-        for(int i = 0; i<indOp; i++){
-            numero1+=numeros.get(i);
-        }
-        for(int i = indOp; i<numeros.size(); i++){
-            numero2+=numeros.get(i);
-        }
-        Integer n1= Integer.parseInt(numero1);
-        Integer n2 = Integer.parseInt(numero2);
-        resultado = n1*n2;
-
-        Character a = (char) resultado;
-        return resultado;
-    }
-    public int div(ArrayList<Character> numeros, int indOp){
-
-        String numero1="";
-        String numero2="";
-        int resultado = 0;
-
-        for(int i = 0; i<indOp; i++){
-            numero1+=numeros.get(i);
-        }
-        for(int i = indOp; i<numeros.size(); i++){
-            numero2+=numeros.get(i);
-        }
-        Integer n1= Integer.parseInt(numero1);
-        Integer n2 = Integer.parseInt(numero2);
-        resultado = n1/n2;
-
-        Character a = (char) resultado;
-        return resultado;
-    }
-
-    //METODO INTERNET
 
     public static double eval(final String str) {
         return new Object() {
@@ -202,8 +95,8 @@ public class MainActivity extends AppCompatActivity {
             double parseExpression() {
                 double x = parseTerm();
                 for (; ; ) {
-                    if (eat('+')) x += parseTerm(); // addition
-                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    if (eat('+')) x += parseTerm();
+                    else if (eat('-')) x -= parseTerm();
                     else return x;
                 }
             }
@@ -211,25 +104,25 @@ public class MainActivity extends AppCompatActivity {
             double parseTerm() {
                 double x = parseFactor();
                 for (; ; ) {
-                    if (eat('X')) x *= parseFactor(); // multiplication
-                    else if (eat('/')) x /= parseFactor(); // division
+                    if (eat('X')) x *= parseFactor();
+                    else if (eat('/')) x /= parseFactor();
                     else return x;
                 }
             }
 
             double parseFactor() {
-                if (eat('+')) return +parseFactor(); // unary plus
-                if (eat('-')) return -parseFactor(); // unary minus
+                if (eat('+')) return +parseFactor();
+                if (eat('-')) return -parseFactor();
 
                 double x;
                 int startPos = this.pos;
                 if (eat('(')) { // parentheses
                     x = parseExpression();
                     if (!eat(')')) throw new RuntimeException("Missing ')'");
-                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') {
                     while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
                     x = Double.parseDouble(str.substring(startPos, this.pos));
-                } else if (ch >= 'a' && ch <= 'z') { // functions
+                } else if (ch >= 'a' && ch <= 'z') {
                     while (ch >= 'a' && ch <= 'z') nextChar();
                     String func = str.substring(startPos, this.pos);
                     if (eat('(')) {
@@ -247,9 +140,6 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     throw new RuntimeException("Unexpected: " + (char) ch);
                 }
-
-                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
-
                 return x;
             }
         }.parse();
